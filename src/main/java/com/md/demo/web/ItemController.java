1008f4ca603479d762b471bf09194ea86e35f4c0
Elitebook <ion.isac94@gmail.com>
2020-02-09 18:33:36 +0200

    feature/DTO added logback.xml file, adjusted pom.mxl file

--- src/main/java/com/md/demo/web/ItemController.java
+++ src/main/java/com/md/demo/web/ItemController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 public class ItemController {
